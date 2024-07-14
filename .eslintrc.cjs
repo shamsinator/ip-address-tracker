ae0733ecb4dd2e3dfae92004b80c65149f452e61
shamsinator <shamsi.amir@gmail.com>
2024-07-14 14:21:32 +0200

    fix: eslint configurations and modifications for the entire project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,36 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  root: true, // Indicates this configuration applies to the root directory and its subdirectories
+  env: {
+    browser: true, // Enables browser globals like `window` and `document`
+    es2020: true, // Enables ES2020 global variables and ES2020 syntax
+    node: true // Enables Node.js global variables and Node.js specific rules
+  },
+  parserOptions: {
+    sourceType: 'module', // Specifies that the code is in ECMAScript module format
+    ecmaVersion: 2020, // Specifies the ECMAScript version to ES2020 (ES11)
+    ecmaFeatures: {
+      jsx: true // Enables JSX syntax parsing
+    }
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
+    'eslint:recommended', // Uses ESLint's recommended rules for all ESLint core rules
+    'plugin:@typescript-eslint/recommended', // Uses recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:react-hooks/recommended', // Uses recommended rules from eslint-plugin-react-hooks plugin
+    'prettier' // Integrates Prettier's formatting rules to ESLint
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'prettier', 'react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'], // Ignores files matching these patterns from linting
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser for TypeScript files
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-refresh'], // Specifies ESLint plugins to use
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true } // Warns if React components are not exported as constants
     ],
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'error' // Displays Prettier errors as ESLint errors
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
+      version: 'detect' // Automatically detects the React version used in the project
+    }
+  }
 }
